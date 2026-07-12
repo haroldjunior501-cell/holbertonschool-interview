e0caf1856a63cc1f6723f60042264497c71c1eb9
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-07-12 13:15:30 +0200

    Optimize makeChange to use greedy algorithm and add README

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """
-Making Change
+Change comes from within
 """
 
 
 def makeChange(coins, total):
     """
-    Determines the fewest number of coins needed to meet a given total.
-
-    Args:
-        coins: list of coin values
-        total: amount to reach
-
-    Return:
-        Fewest number of coins needed, or -1 if impossible
+    Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total.
     """
-
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if coin <= amount:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    if dp[total] == float('inf'):
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        count += total // coin
+        total %= coin
+    if total != 0:
         return -1
-
-    return dp[total]
+    return count
---
+++ making_change/README.md
@@ -0,0 +1,9 @@
+# Making Change
+
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+
+## Files
+
+| File | Description |
+|------|-------------|
+| `0-making_change.py` | Python script that contains the function `makeChange` to calculate the minimum coins required |
