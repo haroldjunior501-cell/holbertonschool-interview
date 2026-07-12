eebc053271ee6b04963ac3940876777babb70b26
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-07-12 13:22:06 +0200

    Fix optimality with hybrid greedy-DP and resolve PEP8

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
 """
-Change comes from within
+Making Change Module
 """
 
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number
-    of coins needed to meet a given amount total.
+    Determines the fewest number of coins needed to meet a given total.
     """
     if total <= 0:
         return 0
-    coins.sort(reverse=True)
-    count = 0
-    for coin in coins:
-        if total <= 0:
-            break
-        count += total // coin
-        total %= coin
-    if total != 0:
+    coins = sorted(list(set(coins)))
+    if not coins:
         return -1
-    return count
+    largest_coin = coins[-1]
+    buffer = largest_coin * 2
+    if len(coins) > 1:
+        buffer = largest_coin * coins[-2]
+    if total > buffer:
+        k = (total - buffer) // largest_coin
+        total = total - k * largest_coin
+    else:
+        k = 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            val = dp[amount - coin] + 1
+            if val < dp[amount]:
+                dp[amount] = val
+    if dp[total] == float('inf'):
+        return -1
+    return dp[total] + k
